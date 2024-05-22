68acfb6c1fcd6f0b58d177b2413d355c10c136fc
Ekaterina <ekaterina_rina01@mail.ru>
2024-05-23 00:32:07 +0300

    add bisnesslogika

---
+++ EmailSender.Business/EmailSender.cs
@@ -0,0 +1,70 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSender.Business;
+
+public class EmailSender : IEmailSender
+{
+    private readonly EmailConfiguration _emailConfig;
+
+    public EmailSender(EmailConfiguration emailConfig)
+    {
+        _emailConfig = emailConfig;
+    }
+
+    public void SendEmail(Message message)
+    {
+        var emailMessage = CreateEmailMessage(message);
+
+        Send(emailMessage);
+    }
+    private MimeMessage CreateEmailMessage(Message message)
+    {
+        var emailMessage = new MimeMessage();
+        emailMessage.From.Add(new MailboxAddress(_emailConfig.From));
+        emailMessage.To.AddRange(message.To);
+        emailMessage.Subject = message.Subject;
+        emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+
+        return emailMessage;
+    }
+
+    private void Send(MimeMessage mailMessage)
+    {
+        var client = new SmtpClient();
+
+        try
+        {
+            client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, true);
+            client.AuthenticationMechanisms.Remove("XOAUTH2");
+            client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
+
+            client.Send(mailMessage);
+        }
+        catch (Exception ex)
+        {
+            //log an error message or throw an exception or both.
+            Console.WriteLine($"Error sending email: {ex.Message}");
+            throw;
+        }
+        finally
+        {
+            if (client.IsConnected)
+            {
+                client.Disconnect(true);
+            }
+            client.Dispose();
+        }
+
+        if (!client.IsConnected)
+        {
+            Console.WriteLine("Email was not sent");
+        }
+    }
+
+}
--- EmailSender/Program.cs
+++ EmailSender/Program.cs
@@ -1,7 +1,13 @@
 using EmailSender;
+using EmailSender.Business;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
 host.Run();
+var emailConfig = builder.Configuration
+        .GetSection("EmailConfiguration")
+        .Get<EmailConfiguration>();
+builder.Services.AddSingleton(emailConfig);
+
