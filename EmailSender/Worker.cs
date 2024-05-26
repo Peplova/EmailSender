8fff3176a55b149f03df35e75b0d419592703930
xnesart <xnesart@mail.ru>
2024-05-26 11:10:43 +0300

    removed try catch from worker class

--- EmailSender/Worker.cs
+++ EmailSender/Worker.cs
@@ -21,23 +21,15 @@ namespace EmailSender
                 if (_logger.IsEnabled(LogLevel.Information))
                 {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                    
-                    try
-                    {
-                        var message = new Message(new string[] { "xnesart@mail.ru" }, "Test email", "Hello.");
-                        var isSending = _mailProcessor.ProcessEndSendMail(message);
-                    
-                        if (isSending) _logger.LogInformation("Email sent in: {time}", DateTimeOffset.Now);
-                        else _logger.LogError("Email sent in: {time}", DateTimeOffset.Now);
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogCritical(ex, "An unexpected error occurred while sending email.");
-                        throw;
-                    }
-                }
 
-                await Task.Delay(5000, stoppingToken);
+                    var message = new Message(new string[] { "xnesart@mail.ru" }, "Test email", "Hello.");
+                    var isSending = _mailProcessor.ProcessEndSendMail(message);
+
+                    if (isSending) _logger.LogInformation("Email sent in: {time}", DateTimeOffset.Now);
+                    else _logger.LogError("Email sent error in: {time}", DateTimeOffset.Now);
+
+                    await Task.Delay(5000, stoppingToken);
+                }
             }
         }
     }
