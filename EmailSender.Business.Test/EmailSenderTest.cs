077e07c5e5ab772b4e814e6ab6fac78ddd6ba164
Ekaterina <ekaterina_rina01@mail.ru>
2024-05-30 01:15:03 +0300

    fix test

--- EmailSender.Business.Test/EmailSenderTest.cs
+++ EmailSender.Business.Test/EmailSenderTest.cs
@@ -17,13 +17,13 @@ public class EmailSenderTest
 
 
     private readonly Mock<IOptionsMonitor<EmailConfiguration>> _configEmailMock;
-    private readonly Mock<ILogger<IEmailSender>> _loggerMock;
+    private readonly Mock<ILogger<MailSender.EmailSender>> _loggerMock;
     private readonly Mock<ISmtpClient> _smtpClientMock;
 
     public EmailSenderTest()
     {
         _configEmailMock = new Mock<IOptionsMonitor<EmailConfiguration>>();
-        _loggerMock = new Mock<ILogger<EmailSender.Business.MailSender.IEmailSender>>();
+        _loggerMock = new Mock<ILogger<MailSender.EmailSender>>();
         _smtpClientMock = new Mock<ISmtpClient>();
     }
 
@@ -31,14 +31,8 @@ public class EmailSenderTest
     public void SendEmail_SuccessSend_Recerved()
     {
         // Arrange
-        var sender = new MailSender.EmailSender(_configEmailMock.Object, (ILogger<MailSender.EmailSender>)_loggerMock.Object, _smtpClientMock.Object);
-        var message = new Message
-        {
-            To = new List<MailboxAddress> { new MailboxAddress("recipient", "recipient@mail.ru") },
-            Subject = "Test Subject",
-            Content = "Test Content"
-        };
-        
+        var sender = new MailSender.EmailSender(_configEmailMock.Object, _loggerMock.Object, _smtpClientMock.Object);
+        var message = new Message(new[] { "to@example.com" }, "Test Subject", "Test Content");
         var emailConfig = new EmailConfiguration
         {
             SmtpServer = "smtp.example.com",
@@ -54,15 +48,43 @@ public class EmailSenderTest
 
         // Assert
         Assert.True(result);
-        _smtpClientMock.Verify(
-            x => x.Connect(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<bool>()),
-            Times.Once);
-        _smtpClientMock.Verify(
-            x => x.Authenticate(It.IsAny<string>(), It.IsAny<string>()),
-            Times.Once);
+        _smtpClientMock.Verify( x => x.Connect(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<bool>()),Times.Once);
+        _smtpClientMock.Verify(x => x.Authenticate(It.IsAny<string>(), It.IsAny<string>()),Times.Once);
         _smtpClientMock.Verify(x => x.Send(It.IsAny<MimeMessage>()), Times.Once);
+ 
+    }
+    [Fact]
+    public void SendEmail_FailureSend_Recerved()
+    {
+        // Arrange
+        var sender = new MailSender.EmailSender(_configEmailMock.Object, _loggerMock.Object, _smtpClientMock.Object);
+        var message = new Message(new[] { "to@example.com" }, "Test Subject", "Test Content");
+        var emailConfig = new EmailConfiguration
+        {
+            SmtpServer = "smtp.example.com",
+            Port = 587,
+            UserName = "testuser",
+            Password = "testpassword",
+            From = "sender@example.com"
+        };
+
+        _configEmailMock.Setup(x => x.CurrentValue).Returns(emailConfig);
+
+        // Act
+        var result = sender.SendEmail(message);
+
+        // Assert
+        Assert.False(result);
+        _smtpClientMock.Verify(x => x.Connect(It.IsAny<string>(), It.IsIn<int>(), It.IsAny<bool>()),
+            Times.Once);
+        _smtpClientMock.Verify(x => x.Authenticate(It.IsIn<string>(), It.IsAny<string>()),Times.Once);
+        _smtpClientMock.Verify(x => x.Send(It.IsAny<MimeMessage>()), Times.Never);
+        _loggerMock.Verify(
+            x => x.LogError(It.IsAny<string>()),
+            Times.Once);
     }
 }
+
 
 
 
