f21b0c74be1ec8da5d0cfa52c7ee512efb831fed
xnesart <xnesart@mail.ru>
2024-06-15 19:59:24 +0300

    sent messaging using MassTransit  for receive is complete

--- EmailSender/Program.cs
+++ EmailSender/Program.cs
@@ -2,12 +2,28 @@ using EmailSender;
 using EmailSender.Business.MailConfiguration;
 using EmailSender.Business.MailProcessor;
 using EmailSender.Business.MailSender;
+using EmailSender.Business.Rabbit;
 using MailKit.Net.Smtp;
+using MassTransit;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
 builder.Services.Configure<EmailConfiguration>(builder.Configuration.GetSection("EmailConfiguration")); 
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<MailRequestConsumer>();
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("rabbitmq://localhost");
+
+        cfg.ReceiveEndpoint("mail_shared", e =>
+        {
+            e.ConfigureConsumer<MailRequestConsumer>(context);
+        });
+    });
+});
+
 builder.Services.AddSingleton<IEmailSender, EmailSender.Business.MailSender.EmailSender>();
 builder.Services.AddSingleton<ISmtpClient, SmtpClient>();
 builder.Services.AddSingleton<IMailProcessor, MailProcessor>();
